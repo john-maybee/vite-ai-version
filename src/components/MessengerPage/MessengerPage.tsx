a14abb09e936c0eeda9c3dbd30e20109c6ffca7a
John Maybee <johnmaybee@Johns-MacBook-Pro.local>
2024-04-23 14:46:41 -0400

    created some component folders and started the messenger page

---
+++ src/components/MessengerPage/MessengerPage.tsx
@@ -0,0 +1,11 @@
+function MessengerPage() {
+    return (
+        <div className="messengerContainer">
+            <div className="messengerHeader">
+                <h1>How can I assist you?</h1>
+            </div>
+        </div>
+    );
+}
+
+export default MessengerPage;
